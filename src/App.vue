d8e2dc92a07d593ce3a2fcf49611b9263f01dd36
Aqcee <nikitashtankowebdes@gmail.com>
2019-04-07 14:47:28 +0400

    add sass

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,7 @@ import HelloWorld from './components/HelloWorld.vue';
 export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+#app
+  +flexbox()
 </style>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'sass',
+      patterns: [
+      	'C:\\work\\vue\\broadcast\\src\\styles\\app.sass',
+      ]
+    }
+  }
+}
